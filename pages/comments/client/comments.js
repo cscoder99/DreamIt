d2ee4aa49e3132614533180bceb9709f6fe28823
Lena Truong <lenatrng938@gmail.com>
2016-10-19 14:41:01 -0400

    comments

--- pages/comments/client/comments.js
+++ pages/comments/client/comments.js
@@ -2,6 +2,10 @@ Template.comments.events({
 "click #submitcomment": function(event){
 	console.log("hey we clicked it");
 	alert("Your comment has been successfully recorded.")
+	const comment =$("#comment").val();
+	const date = new Date();
+	const jsonobj = {comment:comment, date:date};
+	Comments.insert(jsonobj);
 }
 })
 
