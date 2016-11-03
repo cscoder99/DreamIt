9d56a67f2114c97e619de1cd4700446a675a373f
Lena Truong <lenatrng938@gmail.com>
2016-11-03 12:25:28 -0400

    supporter profile page

---
+++ pages/supporters/client/supporterpage.js
@@ -0,0 +1,11 @@
+Template.supporterpage.helpers({
+	compensation:function() {
+		if (this.able) {
+			return "Able to provide compensation"
+		}
+		else {
+			return "Unable to provide compensation"
+		}
+	}
+	
+})
